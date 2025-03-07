8ea8a456df1e21eff6a853090b50b78ab986af7c
hpeter19 <njoshpitar@gmail.com>
2025-03-07 12:13:46 +0300

    MY FIRST REACT APPLICATION

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+  import Header from './header.jsx'
+  import Footer from './footer.jsx'
+  import Food from './food.jsx'
+
+  function App() {
+    return (
+      <>
+        <Header></Header>
+        <Food></Food>
+        <Footer></Footer>
+      </>
+    );
+    
+  }
+
+  export default App
