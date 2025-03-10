96a1a74f799e39a46640a8e651397bd1735831c2
hpeter19 <njoshpitar@gmail.com>
2025-03-10 16:06:32 +0300

    craeting cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import Card from './card'; // Import the Card component
+import Card from './Card'; // Import the Card component
 
 function App() {
   return (
     <div>
+      <Card />
+      <Card />
+      <Card />
       <Card /> {/* Render the Card component */}
     </div>
   );
--- src/card.jsx
+++ src/card.jsx
@@ -1,11 +1,12 @@
+import profilePic from './assets/reactone.jpg'
 import React from 'react';
 
 function Card() {
   return (
     <div className="card">
-      <img src="path_to_image.jpg" alt="Card Image" />
-      <h2>Arcade Holdings</h2>
-      <p>We sell Property At affordable prices</p>
+      <img className="cardimage2 "  src={ profilePic} alt="profile picture" />
+      <h2 className="card-title"> Arcade Holdings</h2>
+      <p className="card-writing">We sell Property At affordable prices</p>
     </div>
   );
 }
