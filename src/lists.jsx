9b7bfd79d70f4405e7ee4a04dca0251d71f6aab5
hpeter19 <njoshpitar@gmail.com>
2025-03-14 09:50:51 +0300

    using id to uniquely identify items in a list

--- src/lists.jsx
+++ src/lists.jsx
@@ -4,13 +4,15 @@ import { useLayoutEffect } from "react";
 function List(){
 //adding curly brackets to arrays making them Objects
 const fruits =[{name:"Apples", calories:95},
-    {name:"Mangoes", calories:45},
-    {name:"Strawberry",calories:88},
-    {name:"Cherry", calories:33}]
+    {Id:1,name:"Mangoes", calories:45},
+    {Id:2,name:"Strawberry",calories:88},
+    {Id:3,name:"Cherry", calories:33}]
 // sorting the array. Does not work with numbers
 //fruit object should be enclosed with curly brackets
 fruits.sort();
-const listItems= fruits.map(fruit => <li key ={fruit.name}>{fruit.name}</li>);
+//fruit.name object is used when each list has different name.
+//for the case of a database, we set the key as id.
+const listItems= fruits.map(fruit => <li key ={fruit.Id}>{fruit.name}</li>);
 return(<ul>{listItems}</ul>);
 }
 
