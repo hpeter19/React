2f2a1bc523865662de59c276dbef4a72fe0c34fe
hpeter19 <njoshpitar@gmail.com>
2025-03-14 11:10:01 +0300

    adding another list using pros

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,25 @@ import Student from './students';
 import List from './lists';
 
 function App() {
+  const fruits =[{Id:1,name:"Apples", calories:105},
+    {Id:2,name:"Mangoes", calories:45},
+    {Id:3,name:"Strawberry",calories:88},
+    {Id:4,name:"Cherry", calories:33}]
+
+  const vegetables =[{Id:5,name:"waru", calories:10},
+      {Id:6,name:"Spinash", calories:115},
+      {Id:7,name:"Sukuma",calories:48},
+      {Id:8,name:"Dania", calories:93}]
+  
+
   return (
     <div >
       <Card />
       <Card />
       <Card />
       <Card /> {/* Render the Card component */}
-      <List />
+      <List items={fruits} category="Fruits"/>
+      <List items={vegetables} category="Vegetables"/>
       <Button />
       <Student name="Billy" age={30} isStudent={true}/>
       <Student name="Caith" age={50} isStudent={false}/>
--- src/lists.jsx
+++ src/lists.jsx
@@ -1,32 +1,23 @@
 import { useLayoutEffect } from "react";
 
 
-function List(){
-//adding curly brackets to arrays making them Objects
-const fruits =[{Id:1,name:"Apples", calories:105},
-    {Id:2,name:"Mangoes", calories:45},
-    {Id:3,name:"Strawberry",calories:88},
-    {Id:4,name:"Cherry", calories:33}]
-// sorting the array. Does not work with numbers
-//fruit object should be enclosed with curly brackets
-//sorting array.a rep 1st item while b rep 2nd item
-//fruits.sort((a,b) => a.name.localeCompare(b.name));//Alphabetical Order
-//fruits.sort((a,b) => b.name.localeCompare(a.name));//reverse order
-//fruits.sort((a,b) => a.calories-b.calories);//numerical order
-fruits.sort((a,b) => b.calories-a.calories);//reversed numerical order
-//low calory fruits
-//const LowCalFruits = fruits.filter(fruit => fruit.calories <100);//low calory fruits
-const HighCalFruits = fruits.filter(fruit => fruit.calories >=100);//High calories fruits
-
-//fruit.name object is used when each list has different name.
-//for the case of a database, we set the key as id.
+function List(props){
 
 
-const listItems=HighCalFruits.map(HighCalFruit => <li key ={HighCalFruit.Id}>
-                     {HighCalFruit.name}:&nbsp;
-                     <b>{HighCalFruit.calories};</b>
+const category = props.category;
+const itemList=props.items;
+
+const listItems=itemList.map(item => <li key ={item.Id}>
+                     {item.name}:&nbsp;
+                     <b>{item.calories};</b>
                      </li>);
-return(<ol>{listItems}</ol>);
+return(
+  <>
+    <h3>{category}</h3>
+    <ol>{listItems}</ol>
+  </>
+
+);
 }
 
 export default List
