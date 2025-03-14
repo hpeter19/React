c01d6f04acbaaae42f2ccb4897da46376e05d74f
hpeter19 <njoshpitar@gmail.com>
2025-03-14 09:38:03 +0300

    sorting arrays before listing them

--- src/lists.jsx
+++ src/lists.jsx
@@ -4,7 +4,9 @@ import { useLayoutEffect } from "react";
 function List(){
 
 const fruits =["Apples","Mangoes","Strawberry","Cherry"]
+// sorting the array. Does not work with numbers
 //fruit object should be enclosed with curly brackets
+fruits.sort();
 const listItems= fruits.map(fruit => <li>{fruit}</li>);
 return(<ul>{listItems}</ul>);
 }
