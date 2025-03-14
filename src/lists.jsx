aabb9ab120a536d670a68c24cccd83682747a24e
hpeter19 <njoshpitar@gmail.com>
2025-03-14 10:17:14 +0300

    low calory fruits in a list

--- src/lists.jsx
+++ src/lists.jsx
@@ -3,7 +3,7 @@ import { useLayoutEffect } from "react";
 
 function List(){
 //adding curly brackets to arrays making them Objects
-const fruits =[{Id:1,name:"Apples", calories:95},
+const fruits =[{Id:1,name:"Apples", calories:105},
     {Id:2,name:"Mangoes", calories:45},
     {Id:3,name:"Strawberry",calories:88},
     {Id:4,name:"Cherry", calories:33}]
@@ -12,12 +12,19 @@ const fruits =[{Id:1,name:"Apples", calories:95},
 //sorting array.a rep 1st item while b rep 2nd item
 //fruits.sort((a,b) => a.name.localeCompare(b.name));//Alphabetical Order
 //fruits.sort((a,b) => b.name.localeCompare(a.name));//reverse order
-fruits.sort((a,b) => a.calories-b.calories)
+//fruits.sort((a,b) => a.calories-b.calories);//numerical order
+fruits.sort((a,b) => b.calories-a.calories);//reversed numerical order
+//low calory fruits
+const LowCalFruits = fruits.filter(fruit => fruit.calories <100);//low calory fruits
+
+
 //fruit.name object is used when each list has different name.
 //for the case of a database, we set the key as id.
-const listItems= fruits.map(fruit => <li key ={fruit.Id}>
-                     {fruit.name}:&nbsp;
-                     <b>{fruit.calories};</b>
+
+
+const listItems=LowCalFruits.map(LowCalFruit => <li key ={LowCalFruit.Id}>
+                     {LowCalFruit.name}:&nbsp;
+                     <b>{LowCalFruit.calories};</b>
                      </li>);
 return(<ol>{listItems}</ol>);
 }
