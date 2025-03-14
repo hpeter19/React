e5925880f92944b468266fdf1a2f58f40ac25ead
hpeter19 <njoshpitar@gmail.com>
2025-03-14 09:34:22 +0300

    Adding items on a list using javascript

--- src/lists.jsx
+++ src/lists.jsx
@@ -4,8 +4,8 @@ import { useLayoutEffect } from "react";
 function List(){
 
 const fruits =["Apples","Mangoes","Strawberry","Cherry"]
-//fruit object should be enclosed with brackets
-const listItems= fruits.map(fruit => <li>(fruit)</li>);
+//fruit object should be enclosed with curly brackets
+const listItems= fruits.map(fruit => <li>{fruit}</li>);
 return(<ul>{listItems}</ul>);
 }
 
