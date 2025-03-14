3ac2ccb97f93b2beb65e19755b08c5651fa53d60
hpeter19 <njoshpitar@gmail.com>
2025-03-14 09:49:01 +0300

    key of items in a list

--- src/lists.jsx
+++ src/lists.jsx
@@ -2,12 +2,15 @@ import { useLayoutEffect } from "react";
 
 
 function List(){
-
-const fruits =["Apples","Mangoes","Strawberry","Cherry"]
+//adding curly brackets to arrays making them Objects
+const fruits =[{name:"Apples", calories:95},
+    {name:"Mangoes", calories:45},
+    {name:"Strawberry",calories:88},
+    {name:"Cherry", calories:33}]
 // sorting the array. Does not work with numbers
 //fruit object should be enclosed with curly brackets
 fruits.sort();
-const listItems= fruits.map(fruit => <li>{fruit}</li>);
+const listItems= fruits.map(fruit => <li key ={fruit.name}>{fruit.name}</li>);
 return(<ul>{listItems}</ul>);
 }
 
