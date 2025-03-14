571075aa03f2f1c081a2627c932feae6d0b83cb5
hpeter19 <njoshpitar@gmail.com>
2025-03-14 09:30:57 +0300

    list

--- src/lists.jsx
+++ src/lists.jsx
@@ -4,7 +4,8 @@ import { useLayoutEffect } from "react";
 function List(){
 
 const fruits =["Apples","Mangoes","Strawberry","Cherry"]
-const listItems= fruits.map(fruit => <li>fruit</li>);
+//fruit object should be enclosed with brackets
+const listItems= fruits.map(fruit => <li>(fruit)</li>);
 return(<ul>{listItems}</ul>);
 }
 
