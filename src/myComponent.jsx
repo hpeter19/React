455eaa19711d3f82939e9f332c51bfefadbc78c1
hpeter19 <njoshpitar@gmail.com>
2025-03-18 13:59:14 +0300

    incrementing age

--- src/myComponent.jsx
+++ src/myComponent.jsx
@@ -1,19 +1,26 @@
 import React,{useState} from 'react';
 
 function MyComponent(){
-#response button with name appearing after clicking
 const [name,setName]= useState();
-
+const [age,setAge] = useState(0); //setting age component to increment age
+//age is set to initially 0 after which its incremenented
 
 const updateName = () => {
 
     setName("mark");
-    
-}
+}    
+const incrementAge = () => {
+    setAge(age + 1);
+
+}   
 
 return(<div>
-<p>name: {name}</p>
-<button onClick={updateName}>set Name</button>
+    <p>Name: {name}</p>
+    <button onClick={updateName}>set Name</button>
+
+
+    <p>Age: {age}</p>
+    <button onClick= {incrementAge} > Increment Age </button>
 
 </div>);
 
