b9aea6978931122d6295cb29288864b9d5f2dd4a
hpeter19 <njoshpitar@gmail.com>
2025-03-19 10:31:49 +0300

    adding a text area

--- src/myComponent.jsx
+++ src/myComponent.jsx
@@ -2,13 +2,20 @@ import React,{useState} from 'react';
 
 
 function MyComponent(){
+    //function of the user
 const[name,setName] = useState("Guests");
+//function of the quantity 
 const[quantity,setQuantity] =useState(1);
+//Comment function
+const[comment,setComment]= useState("");
  function handleNameChange(event){
     setName(event.target.value);
  }
  function handleQuantityChange(event){
     setQuantity(event.target.value);
+ }
+ function handleCommentChange(event){
+    setComment(event.target.value);
  }
 
 return(<div>
@@ -16,6 +23,10 @@ return(<div>
     <p>Name:{name}</p>
     <input value={quantity} onChange={handleQuantityChange} type='number' />
     <p>Quantity: {quantity}</p>
+    //placeholder holds the initial comment in the textarea
+    <input  value={comment} onChange={handleCommentChange} 
+    placeholder='provide delivery instructions'/>
+    <p>Comment: {comment}</p>
       </div>);
 }
 
