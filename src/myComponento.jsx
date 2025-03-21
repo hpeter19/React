d09e44f6fe3bbebbf18f876c6a357c6e0dbd5ea2
hpeter19 <njoshpitar@gmail.com>
2025-03-21 17:31:19 +0300

    list incrementing

--- src/myComponento.jsx
+++ src/myComponento.jsx
@@ -1,23 +1,23 @@
-    import React,{useState} from 'react';
+import React,{useState} from 'react';
 //importing the source objects
-        function MyComponento(){
+function MyComponento(){
 
-        const [car,setCar]= useState({Year:2025,Manufacturer:"Ford",Model:"Raptor"}); 
-        function handleYearChange(event){
-            setCar(c=> ({...c, Year:event.target.value}));
-        }    
-        function handleManufacturerChange(event){
-                setCar(c => ({...c, Manufacturer:event.target.value}));   
-        }   
-        function handleModelChange(event){
-            setCar(c=>({...c, Model:event.target.value}));
+        const[food,setFood]=useState(["apple","Orange","Mangoe"]);
+
+        function addHandleFood(){
+
         }
-        return( <div>
-        <p>Your favourite car is:{car.Year} {car.Manufacturer} {car.Model}, </p>
+        
+        function addRemoveFood(){
+            
+        }
+        //adding list to the components
+    return(<div>
+        <h2>List Of Food :</h2>
+        <ul>
+            {food.map((food, index) => <li key={index}>{food}</li>)}
+        </ul>
+    </div>);
 
-        <input type="number" value={car.Year} onChange={handleYearChange}/><br/>
-        <input type="text" value={car.Manufacturer} onChange={handleManufacturerChange}/><br/>
-        <input type="text" value={car.Model} onChange={ handleModelChange}/>
-            </div>);
-    }
-    export default MyComponento
+}
+export default MyComponento
