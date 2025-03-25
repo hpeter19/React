d75c495534b088d33f1218c60be47f6f9f09391f
hpeter19 <njoshpitar@gmail.com>
2025-03-25 18:03:20 +0300

    adding the title upon rendering

--- src/myCompnent.jsx
+++ src/myCompnent.jsx
@@ -4,17 +4,24 @@ function Mycompnent(){
 
      const [count ,setCount]= useState(0);
 
-     useEffect(function, [dependencies]);
+     useEffect(() => {
+        document.title=`count: ${count}`;
+     },[count]);
      
-//adding functionality to count to increase previous state by 1
+
      function addCount(){
         setCount(c => c +1 )
      }
+     function subtractCount(){
+        setCount(c => c -1 )
+     }
+     //changing the title of the document
 
 return(<>
     
     <p>Count:{count}</p>
     <button onClick={addCount}>Add</button>
+    <button onClick={subtractCount}>Subtract</button>
       </>);
 }
 export default Mycompnent
