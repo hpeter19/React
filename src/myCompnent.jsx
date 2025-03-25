905ca445c23ff64e02e87ed6a6131194e13b7298
hpeter19 <njoshpitar@gmail.com>
2025-03-25 18:13:07 +0300

    setting the color of paragraph to be the color chosen

--- src/myCompnent.jsx
+++ src/myCompnent.jsx
@@ -3,6 +3,8 @@ import  React,{ useState ,useEffect } from "react"
 function Mycompnent(){
 
      const [count ,setCount]= useState(0);
+     const [color ,setColor]=useState("red");
+
 
      useEffect(() => {
         document.title=`count: ${count}`;
@@ -15,13 +17,16 @@ function Mycompnent(){
      function subtractCount(){
         setCount(c => c -1 )
      }
-     //changing the title of the document
+    function changeColor(){
+        setColor(c=> c === "red"? "green" :"red");
+    }
 
 return(<>
     
-    <p>Count:{count}</p>
+    <p style={{color:color}}>Count:{count}</p>
     <button onClick={addCount}>Add</button>
-    <button onClick={subtractCount}>Subtract</button>
+    <button onClick={subtractCount}>Subtract</button><br/>
+    <button onClick={changeColor}>ChangeColor</button>
       </>);
 }
 export default Mycompnent
