ec88b3135992ff095439c1df57e47ca69db13930
hpeter19 <njoshpitar@gmail.com>
2025-03-26 18:11:10 +0300

    time rendering function

--- src/digitalClock.jsx
+++ src/digitalClock.jsx
@@ -4,7 +4,7 @@ import React, {useState,useEffect} from 'react';
 function DigitalClock(){
 
        const[time,setTime]=useState(new Date());
-//when we mount create an interval that updates after every one seconds
+//when we mount create an interval that updates after every one
        useEffect(() =>{
          const intervalId= setInterval(()=>{
             setTime(new Date())
@@ -14,14 +14,23 @@ function DigitalClock(){
             clearInterval(intervalId)
          }
        },[]);
+       //function to format time
     function formatTime(){
+         let hours = time.getHours();
+         const minutes=time.getMinutes();
+         const seconds=time.getSeconds();
+         const meridian= hours >=12 ?"PM": "AM"
+
+         hours = hours % 12 || 12;
+
+         return `${hours}:${minutes}:${seconds} ${meridian}`
 
     }
 
     
     return(<div className='clock-container'>
               <div className='clock'>
-                <span>00:00:00</span>
+                <span>{formatTime}</span>
 
               </div>
 
